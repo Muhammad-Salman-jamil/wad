027c2fbeb2cec4eab4d938f8721e80a186f68a36
Muhammad-Salman-jamil <40597824+Muhammad-Salman-jamil@users.noreply.github.com>
2018-11-25 10:04:21 -0800

    Add files via upload

---
+++ salman/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap4.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main_2.css">
+	<script src="js/main2.js"></script> 	
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-sm nav1">
+	<div class="container">
+		<div class="col-sm-9">
+			<p> UCP Johar Town, Lahore | Call us on: <font color="blue">+92 00 0000 0000</font></p>		
+		</div>
+		<div class="col-sm-3">
+			<p>Our team | FAQ &nbsp&nbsp
+				<a href="#" class="fa fa-facebook icon"></a>&nbsp
+				<a href="#" class="fa fa-twitter icon"></a>&nbsp
+				<a href="#" class="fa fa-linkedin icon"></a>&nbsp
+				<a href="#" class="fa fa-pinterest icon"></a>&nbsp
+			</p>
+		</div>
+	</div>
+</nav>
+
+
+<div class="container-fluid img">
+	<div class="row" style="padding-top: 10px;">
+		<div class="col-sm-1">
+			<img src="images/logo.jpg" class="logo">
+		</div>
+		<div class="col-sm-2">
+			<h5>University of Central Punjab</h5>
+		</div>
+		<div class="col-sm-3">	</div>
+		<div class="col-sm-6">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+          					<li><a href="#">Page 1-1</a></li>
+          					<li><a href="#">Page 1-2</a></li>
+          					<li><a href="#">Page 1-3</a></li>
+        				</ul>
+				</li>
+ 				<li><a href="#">Portfolio</a></li>
+      			<li><a href="#">Blog</a></li>
+      			<li><a href="#">Shop</a></li>
+      			<li><a href="#">Elements</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="container what-to-do">
+	<h1 style="text-align: center;">A Little Bit About Me</h1><br>
+	<div class="row">
+		<div class="col-sm-1"></div>
+		<div class="col-sm-3">
+			<img src="images/Batman_TAS_-_Joker.jpg" style="width: 100%; height: 90%;">
+		</div>
+		<div class="col-sm-7">
+			<h6>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</h6>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit
+			</p>
+			<p>
+				<a href="#" class="fa fa-facebook icon2"></a>&nbsp&nbsp
+				<a href="#" class="fa fa-twitter icon2"></a>&nbsp&nbsp
+				<a href="#" class="fa fa-linkedin icon2"></a>&nbsp&nbsp
+				<a href="#" class="fa fa-pinterest icon2"></a>&nbsp&nbsp
+			</p>
+		</div>
+		<div class="col-sm-1"></div>
+	</div>
+</div>
+
+<div class="container what-to-do">
+	<h1 style="text-align: center;">Say Hello</h1><br>
+	<div class="row">
+		<div class="col-sm-6">
+			<h5> Add a Comment </h5><br>
+			<form action="output.php" method="post" onsubmit="return validate()">
+				<div class="form-group-row">
+					<input type="text" name="fname" id="fname" placeholder="First Name" class="col-sm-4">&nbsp&nbsp&nbsp&nbsp
+					<input type="text" name="lname" id="lname" placeholder="Last Name" class="col-sm-4"><br><br>								 
+				</div>
+				<div class="form-group-row">
+					<input type="email" name="email" id="email" placeholder="Email" class="col-sm-10"><br><br>				
+				</div>
+				<div class="form-group-row">
+					<textarea cols="55" rows="5" name="message" placeholder="Message" class="col-sm-10"></textarea><br><br>					
+				</div>
+				<div class="form-group-row">
+					<button type="submit" class="col-sm-10" name="submit" style="background-color: pink">Submit</button>
+				</div>
+			</form>
+		</div>
+
+		<div class="col-sm-6">
+			<h5>Contact Info</h5>
+			<p>
+				 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><hr>
+				 Address: UCP Johar Town, Lahore<br><hr>
+				 Mobile 1: +92 000 0000<br><hr>
+				 Email:	companyName@yourname<br><hr>
+			</p>
+		</div>
+	</div>
+</div>
+
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-sm-2"></div>
+
+		<div class="col-sm-2" style="margin:auto;">
+			<br><h5>About me</h5><br>
+			<p>BSCS 5th semester student</p>
+			<input type="text" name="">
+		</div>
+
+		<div class="col-sm-2" style="margin:auto;">
+			<br><h5>Connect With Me</h5>
+			<p>
+				Instagram<br>Facebook<br>Twitter<br>Pinterest<br>Vimeo
+			</p>
+		</div>
+
+		<div class="col-sm-2" style="margin:auto;">
+			<br><h5>Additional Links</h5>
+			<p>
+				Projects<br>Pages<br>Shop<br>About<br>Contact
+			</p>
+		</div>
+
+		<div class="col-sm-2" style="margin:auto;">
+			<br><h5>Contact Me</h5><br>
+			UCP Johar Town, Lahore<br>yourname@yoursite.com<br>Call us on:<br>+92 00 0000
+		</div>
+
+		<div class="col-sm-2"></div>
+	</div>
+</footer>
+
+</body>
+</html>
